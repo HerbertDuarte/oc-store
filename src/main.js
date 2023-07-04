fe1bc860f0f37cc14345e7e134a772ce6e45fb8d
Herbert Duarte <herbertduarte.santos@gmail.com>
2023-07-04 10:18:48 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,17 +2,22 @@ import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import './style.css'
 import App from './App.vue'
-import { routes } from './routes'
+import HomeVue from './pages/Home.vue'
+import RamPageVue from './pages/RamPage.vue'
+import SsdPageVue from './pages/SsdPage.vue'
+import PcPageVue from './pages/PcPage.vue'
+import NotFoundVue from './pages/NotFound.vue'
+import KitsPageVue from './pages/KitsPage.vue'
 
 const router = createRouter({
   history : createWebHistory(),
   routes : [
-    {path: '/', name : 'Home', component : import('./pages/Home.vue') , list : true},
-    {path: '/items/ram', name: 'Memória RAM', component: import('./pages/RamPage.vue'), list : true},
-    {path: '/items/ssd', name: 'SSD', component: import('./pages/SsdPage.vue'), list : true},
-    {path: '/items/kits', name: 'Kit Upgrade', component: import('./pages/SsdPage.vue'), list : true},
-    {path: '/items/pc', name : 'PC Completo', component : import('./pages/PcPage.vue') , list : true},
-    {path: '/:pathMatch(.*)*', name: 'NotFound', component: import('./pages/NotFound.vue'), list : false},
+    {path: '/', name : 'Home', component : HomeVue },
+    {path: '/items/ram', name: 'Memória RAM', component: RamPageVue},
+    {path: '/items/ssd', name: 'SSD', component: SsdPageVue},
+    {path: '/items/kits', name: 'Kit Upgrade', component: KitsPageVue},
+    {path: '/items/pc', name : 'PC Completo', component : PcPageVue },
+    {path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFoundVue},
   ]
 })
 
